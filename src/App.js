5f2a3e96a892ad42082e58bcf389a72854694b89
DawidWraga <81368636+DawidWraga@users.noreply.github.com>
2022-05-25 14:06:22 +0100

    Update App.js
    refactor

--- src/App.js
+++ src/App.js
@@ -6,20 +6,6 @@ import InputBar from './components/InputBar';
 import Background from './components/Background';
 import AppContextProvider from './contexts/AppContextProvider';
 
-//////////////////////TODO
-/*
-
-remove 7 items left and instead have a small circle next to [all active complete] (ie displayMode state)  with number insire
-
-Reorder cards functionality
-
-TODO: TECHNOLOGIES
-Testing
-typescript
-redux 
-UML syntax in VSCODE
-*/
-
 function App() {
 	return (
 		<div>
